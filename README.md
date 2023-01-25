45522e90cf7ca93cddc5312924072c2dcc024e19
Kyle Freemantle <kcfreemantle@gmail.com>
2023-01-25 00:07:41 -0800

    I am nowhere near done and I don't think I know how to manage my time.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# about-me
-Code 201 - Lab02 project: About Me page
+# LAB - 02
+
+## aboutMe
+
+I am building a webpage about myself, **Kyle Freemantle**, a student at **Code Fellows** in Seattle, WA.
+
+### Author: Kyle Freemantle
+
+### Links and Resources
+
+- submission PR
+- I relied heavily on guidance on **conditionals** from both [MDN](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals) and [W3 Schools](https://www.w3schools.com/js/js_if_else.asp).
+
+### Lighthouse Accessibility Report Score
+
+- ![lab02-lighthouse-report]().
+
+### Reflections and Comments
+
+- I need to manage my time better.  I am slow and distractable.  This is improving, but I would prefer it improve faster.
+- Conditional logic is becoming more intuitive!
---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+/// scrap code from the lab1 project, included for reference of progress.
+
+// //initial user question workflow, intended to generate FOUR question prompts and store four responses in different variables using the prompt function.
+// let herodotusAge = 54;
+// let userName = prompt("What's your name, junior historian?", "~name~");
+// console.log(userName);
+// let userAge = prompt("And how old are you, " + userName + "?");
+// console.log(userAge);
+// let ageDifferential = herodotusAge - userAge
+// let userHistoricalEvent = prompt("Good, then you've got some time left. What's the earliest historical event you can name?");
+// console.log(userHistoricalEvent);
+// let userBook = prompt("Finally, what was the last GOOD book you read?");
+// console.log(userBook);
+// let userInitResponse = alert("Thanks, " + userName + ". You're " + userAge + " years old, " + (herodotusAge - userAge) + " years younger than Herodotus when he wrote Histories.  How would he have written about " + userHistoricalEvent + "?  Would it have been a better read than " + userBook + "?");
+// console.log(userInitResponse);
+
+// ////  comment out scripting for ease of testing
