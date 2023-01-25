50f1ff47a90b75eb053f475fe82f30623c274f30
Kyle Freemantle <kcfreemantle@gmail.com>
2023-01-25 14:36:03 -0800

    Second attempt at Lab02 site

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
+window.onload = function() {
+  let userName = prompt('What\'s your name, please?');
+console.log(userName);
+
+
+let userBackgroundTech = prompt('Let\'s get to know each other better.  Do you have a background in tech?', 'Yes/No').toLowerCase();
+console.log(userBackgroundTech);
+if (userBackgroundTech === 'yes') {
+  alert('Excellent! Me too, at least adjacently. I came from healthcare IT.');
+} else  alert('That\'s OK, we can learn together.');
+
+let userBackgroundMilitary = prompt('Have you served in the military?', 'Yes/No').toLowerCase();
+console.log(userBackgroundMilitary);
+if (userBackgroundMilitary === 'yes') {
+  alert('Very cool! I was in the Marine Corps before I was in IT.');
+} else alert('Love it. I got lost on the way to college and wound up in the Marines.');
+
+let userCollege = prompt('And did you go to college or a trade school?', 'Yes/No').toLowerCase();
+console.log(userCollege);
+if (userCollege === 'yes') {
+  alert('I hope it was valuable for you! I studied finance and economics at Western Washington University.');
+} else alert('I did, but barely. I often struggled in college.');
+
+let userLiveSeattle = prompt('Do you currently live in Seattle?', 'Yes/No').toLowerCase();
+console.log(userLiveSeattle);
+if (userLiveSeattle === 'yes') {
+  alert('I hope it agrees with you!  I grew up in Washington and moved to Seattle after I finished college.');
+} else alert('Then you\'re probably saving some money!.');
+
+let userWorkSeattle = prompt('If you worked in tech, would you like to work in the Seattle area?', 'Yes/No').toLowerCase();
+console.log(userWorkSeattle);
+if (userWorkSeattle === 'yes') {
+  alert(`It can be a fun place to work, ${userName}! I've worked downtown for several years.`);
+} else alert(`Truthfully ${userName}, I would like to try living somewhere else myself.`);
 
 
 
