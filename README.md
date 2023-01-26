4158fdc59843a418ba3a26fbfa3395241a461536
Kyle Freemantle <kcfreemantle@gmail.com>
2023-01-26 00:02:50 -0800

    Reworked conditional logic for two guessing games with help from Steve Minor.

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 ## aboutMe
 
-I am building a webpage about myself, **Kyle Freemantle**, a student at **Code Fellows** in Seattle, WA.
+I am building a webpage about myself, **Kyle Freemantle**, a student at **Code Fellows** in Seattle, WA.  The objective is to get more practice handling JavaScript and CSS rules while building out functionality on the page.  Features are being added to the page incrementally. 
+
+### Revision History
+
+**1/25:** initial page published with 5 yes/no questions for the user with responses from the site author to get to know each other better.
+**1/25:** second iteration of the page to include a list of favorite games, a numerical guessing game, a muiltiple choice question with 6 possible attempts, and a final score computed for the user. Also structured work history as lists.  In order to complete the if..else statements I got a lot of help from an experience JS dev.
 
 ### Author: Kyle Freemantle
 
@@ -10,12 +15,14 @@ I am building a webpage about myself, **Kyle Freemantle**, a student at **Code F
 
 - [re: Kyle Freemantle site](https://kfreemantle.github.io/about-me/)
 - I relied heavily on guidance on **conditionals** from both [MDN](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/conditionals) and [W3 Schools](https://www.w3schools.com/js/js_if_else.asp).
+- Steve Minor (@thelastbaldwin on GitHub) gave me a lot of hands on help after hours to structure my conditional statements.
 
 ### Lighthouse Accessibility Report Score
 
-- ![lab02-lighthouse-report](lab02lighthouse.png).
+![lab02-lighthouse-report](lab02lighthouse.png).
 
 ### Reflections and Comments
 
 - I need to manage my time better.  I am slow and distractable.  This is improving, but I would prefer it improve faster.
 - Conditional logic is becoming more intuitive!
+- I need to get outside help much faster, but I have some good resources.
--- js/app.js
+++ js/app.js
@@ -34,23 +34,76 @@ if (userWorkSeattle === 'yes') {
 } else alert(`Truthfully ${userName}, I would like to try living somewhere else myself.`);
 
 
+// Age Guessing Game logic
+const kyleAge = 38;
+const maxGuessesAge = 6;       // Using constant variables to set conditions that won't change.  My age is 38, the maximum guesses we have are 6.
+
+let attemptedGuessesAge = 0;
+let guessMessageAge = 'Guess my age in six tries or less!';     // The number of tries the user has made, initially set to zero, and a variable to store the message.
+
+while(attemptedGuessesAge < maxGuessesAge){
+  const userGuessAge = parseInt(prompt(guessMessageAge));
+  attemptedGuessesAge++;       // The while loop runs as long as the attempted guesses are fewer than the maximum guesses. It prompts the user to guess my age, stores the result in a variable, and then increments the attempted guesses up by one.
+
+  if (userGuessAge < kyleAge){
+    guessMessageAge = `That's too low! Guess again.`;
+  } else if (userGuessAge > kyleAge){
+    guessMessageAge = 'Too High! Guess Again';
+  } else if (userGuessAge === kyleAge) {
+    alert(`Correct! You guessed my age in ${attemptedGuessesAge} attempt(s)`);
+    break;
+
+  }  // These if..else statements check the users's guess against the constant of my age. Too low, message says so, move to next condition. If guess is too high, message says so, moves to next condition. If the user guess strictly equals my age, we return an alert that the user got it right and we stop the code with a break.
+}
 
 
+// Array Guessing game logic
+const maxGuessesGames = 6;
 
-/// scrap code from the lab1 project, included for reference of progress.
+let correctCount = 0;
+let attemptedGuessesGames = 0;  // Set a constant for the max guesses, and variables for our initial counters for correct guesses and attempted guesses.
 
-// //initial user question workflow, intended to generate FOUR question prompts and store four responses in different variables using the prompt function.
-// let herodotusAge = 54;
-// let userName = prompt("What's your name, junior historian?", "~name~");
-// console.log(userName);
-// let userAge = prompt("And how old are you, " + userName + "?");
-// console.log(userAge);
-// let ageDifferential = herodotusAge - userAge
-// let userHistoricalEvent = prompt("Good, then you've got some time left. What's the earliest historical event you can name?");
-// console.log(userHistoricalEvent);
-// let userBook = prompt("Finally, what was the last GOOD book you read?");
-// console.log(userBook);
-// let userInitResponse = alert("Thanks, " + userName + ". You're " + userAge + " years old, " + (herodotusAge - userAge) + " years younger than Herodotus when he wrote Histories.  How would he have written about " + userHistoricalEvent + "?  Would it have been a better read than " + userBook + "?");
-// console.log(userInitResponse);
+const kyleGames = [
+  `Dungeon's & Dragons`,
+  `Fallout: New Vegas`,
+  `Fallout 2`,
+  `Deus Ex`,
+  `Morrowind`,
+  `S.T.A.L.K.E.R. - Shadows of Chernobyl`,
+  `Nethack`,
+  `Azul`,
+  `Thornwatch`,
+  `Clank! Legacy`
+];   // declaring a constant for my games list, which is unchanging as far as the user is concerned.
 
-// ////  comment out scripting for ease of testing
+function processGameName(gameName){
+  return gameName.toLowerCase();
+};   // this is a function to process the gameName to lower case, but we may need more validation later so we're parking that processing in this function to make it easier later on.
+
+while (attemptedGuessesGames < maxGuessesGames){
+  const userGuessGame = prompt("Can you guess of my favorite games?");
+  attemptedGuessesGames++;   // the while loop runs as long as our attempted guesses is less than our max guesses.  It prompts the user for a guess, and stores the result a
+
+  if (typeof userGuessGame === 'string'){
+    const lowerGuess = processGameName(userGuessGame);
+    let match = false;
+
+    // check to see if it's in the array
+    for (let i = 0; i < kyleGames.length; i++){
+      let currentGame = processGameName(kyleGames[i]);
+
+      if(lowerGuess === currentGame){
+        match = true;
+        break;
+      }
+    }
+
+    // we know if you were right or wrong
+    if (match){
+      correctCount++;
+    }
+  }
+}
+
+alert(`You guessed ${correctCount} guesses correct. Nicely done!`);
+
